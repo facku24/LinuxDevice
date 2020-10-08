13d7c0e91707447a4a02809f32dbbd2cd39eae1b
facundo <facundo.ramallo@bitlogic.io>
2020-10-07 23:26:25 -0300

    Adding Lesson 4 and 5

--- embetronicx/Lesson5/device_file_driver.c
+++ embetronicx/Lesson5/device_file_driver.c
@@ -1,6 +1,6 @@
 /*************************************************************************************
- * FILE:        dynamic_allocation_module.c
- * DETAILS:     Simple linux driver (dynamic allocating the Major and Minor number)
+ * FILE:        device_file_module.c
+ * DETAILS:     Simple linux driver (Device File Module)
  * AUTHOR:      Kunix
  * COMPILE:  	make
  * CLEAN: 		make clean
@@ -9,6 +9,8 @@
  * VERIFY:		dmesg
  * REMOVE:		sudo rmmod <driver_name> | <driver_name>.ko
  * INFO: 		modinfo <driver_name>.ko
+ * CREATE:      sudo mknod -m <permissions> <name> <device type> <major> <minor>
+ * EXAMPLE:     sudo mknod -m 666 /dev/etx_device c 246 0
  *************************************************************************************/
 #include<linux/kernel.h>
 #include<linux/init.h>
@@ -18,15 +20,12 @@
 
 dev_t dev = 0;
 
-/*
-** Module Init function
-*/
-static int __init dynamic_allocation_init(void)
+static int __init device_file_init(void)
 {
     /*Allocation Major Number*/
-    if((alloc_chrdev_region(&dev, 0, 1, "Kunix_Dev")) < 0)
+    if((alloc_chrdev_region(&dev, 0, 1, "kunix_dev")) < 0)
     {
-        printk(KERN_INFO "Cannot allocate major number for device 1\n");
+        printk(KERN_INFO "Cannot allocate major number for device\n");
         return -1;
     }
     printk(KERN_INFO "Major = %d Minor = %d \n", MAJOR(dev), MINOR(dev));
@@ -35,19 +34,16 @@ static int __init dynamic_allocation_init(void)
     return 0;
 }
 
-/*
-** Module Exit Function
-*/
-static void __exit dynamic_allocation_exit(void)
+void __exit device_file_exit(void)
 {
     unregister_chrdev_region(dev, 1);
     printk(KERN_INFO "Kernel Module Removed Successfully...\n");
 }
 
-module_init(dynamic_allocation_init);
-module_exit(dynamic_allocation_exit);
+module_init(device_file_init);
+module_exit(device_file_exit);
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Kunix <kunix@gmail.com>");
-MODULE_DESCRIPTION("Simple Linux Driver (Dynamic allocation the Major and Minor Number)");
+MODULE_DESCRIPTION("A Simple Driver for Device File Creation");
 MODULE_VERSION("1.1");
